2821f691198138203eea38cfa583d9b17ee7799e
Ryan Broome <ryan@MacBook-Pro.lan>
2023-09-20 13:10:01 -0400

    release exercise pdf to github, delete upon completion

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,15 +1,15 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/** Update company data with `partial data`, .
+ *input { dataToUpdate, jsToSql } => output { setCols: }
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
-  const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
-  );
+  const cols = keys.map((colName, idx) => `"${jsToSql[colName] || colName}"=$${idx + 1}`);
 
   return {
     setCols: cols.join(", "),
