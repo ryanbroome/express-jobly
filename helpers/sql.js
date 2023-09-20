f8847ce969281e4f1d1870025d06478fdb67df76
Ryan <110047631+ryanbroome@users.noreply.github.com>
2023-09-20 13:12:33 -0400

    Update sql.js

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,13 +1,6 @@
 const { BadRequestError } = require("../expressError");
 
 // THIS NEEDS SOME GREAT DOCUMENTATION.
-// Accepts a data object, jsToSql obj
-// creates a keys array of all the keys in the dataToUpdate object
-// checks to make sure there is at least 1 key // aka data 
-//creates a cols array of strings, maps each key passed in dataToUpdate to the cols array as a string like this "first_name"= $1, key = jsToSql[dataToUpdate[value] || value], $1 = $ + (idx + 1)
-// returns an object with two keys 
-// 1 setCols : "first_name = $1" + "dataToUpdate[key2] = $(idx1 + 1) aka $(idx2) aka $(2)"
-// 2 values : (Object.values(dataToUpdate)) aka ["Aliya" aka Object.values(dataToUpdate)[idx0], 32 aka Object.values(dataToUpdate)[idx1]]
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
