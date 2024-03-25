2c6914a416796b14e29a7f18a59734d0d5b9764b
Ryan Broome <ryanjbroome@gmail.com>
2024-03-25 12:57:38 -0400

    db trouble

--- config.js
+++ config.js
@@ -5,7 +5,8 @@
 require("dotenv").config();
 require("colors");
 
-const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
+const SECRET_KEY = process.env.SECRET_KEY || "zHCQxmv752BZk02MBWyUi8IwxC8owrjK";
+// const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
 const PORT = +process.env.PORT || 3001;
 
